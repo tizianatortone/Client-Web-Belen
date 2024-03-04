2af978fbf9fc70ce303856d202cdf571b1b01d1f
tizianatortone <tiziana_tortone@hotmail.com>
2024-03-04 17:59:39 +0100

    navbar hidden on scroll + AOS animation

--- src/script.js
+++ src/script.js
@@ -1,1 +1,20 @@
+let scrollPos = 0;
+const nav = document.querySelector(".container-fluid");
 
+function checkPosition() {
+  let windowY = window.scrollY;
+  if (windowY < scrollPos) {
+    // Scrolling UP
+    nav.classList.add("is-visible");
+    nav.classList.remove("is-hidden");
+  } else {
+    // Scrolling DOWN
+    nav.classList.add("is-hidden");
+    nav.classList.remove("is-visible");
+  }
+  scrollPos = windowY;
+}
+
+window.addEventListener("scroll", checkPosition);
+
+AOS.init();
